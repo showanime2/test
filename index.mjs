37dee907107a01d0076d792fa33b0a0032a0f8a1
showanime2 <supershow420@gmail.com>
2025-04-16 14:33:26 +0530

    Update index.mjs

--- index.mjs
+++ index.mjs
@@ -22,7 +22,6 @@ app.get('/proxy', async (req, res) => {
 
         const response = await fetch(targetUrl, {
             headers: {
-                'Origin': "https://megacloud.club/",
                 'Referer': "https://megacloud.club/",
             },
         });
